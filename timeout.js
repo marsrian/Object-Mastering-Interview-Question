e09cc2aa81c0479eea251231b94ed6513788992f
marsrian <marsrian40@gmail.com>
2022-05-31 02:20:06 +0600

    dateTime

---
+++ timeout.js
@@ -0,0 +1,16 @@
+function doSomething() {
+    console.log(2222);
+}
+console.log(1111);
+// doSomething();
+// setTimeout(() => {
+//     console.log("See you later")
+// }, 4000);
+
+setInterval(function() {
+    console.log('doing it again')
+}, 3000);
+
+console.log(3333);
+console.log(4444);
+console.log(5555);
